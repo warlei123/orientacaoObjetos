96088684707e923f85a0324f6bfb1c513d50916a
warlei123 <warlei.o.santoss@gmail.com>
2021-04-26 11:49:03 -0700

    criado pasta oojs

---
+++ oojs/oojs.js
@@ -0,0 +1,34 @@
+const person = {
+    name: {
+        first: 'Bob',
+        last: 'Smith'
+    },
+    age: 32,
+    gender: 'male',
+    interests: ['music', 'skiing'],
+    bio: function() {
+        alert(`${this.name.first} ${this.name.last} is ${this.age} years old. He likes ${this.interests[0]} and ${this.interests[1]}`)
+    },
+    greeting: function(){
+        alert(`Hi! I\`m ${this.name.first}.`)
+    }
+}
+
+let myDataName = 'height'
+let myDataValue = '1,75m'
+person[myDataName] = myDataValue
+
+
+const person1 = {
+    name: 'Chris',
+    greeting: function() {
+      alert('Hi! I\'m ' + this.name + '.');
+    }
+  }
+  
+  const person2 = {
+    name: 'Deepti',
+    greeting: function() {
+      alert('Hi! I\'m ' + this.name + '.');
+    }
+  }
